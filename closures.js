de514d0ad59751a760e1cb98e3c9f5ac03db6a1f
olli-hash <52583865+olli-hash@users.noreply.github.com>
2019-11-18 19:59:31 +0100

    Update closures.js

--- closures.js
+++ closures.js
@@ -1,3 +1,14 @@
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
+
+
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+
