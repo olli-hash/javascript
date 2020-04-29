1692b0c79f7c059c2f6983b0871f77866a0eb4d6
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-04-29 13:50:57 +0200

    M

--- arrays.js
+++ arrays.js
@@ -6,10 +6,58 @@ var arrays = new (function(a,b){
     
                                     // dynamische Zahlenräume
     
-    var a1 = []
+    
+	
+	var a1 = []				//
     var a2 = [[ ]]
     var a3 = [[[ ]]]
     var a4 = [[[[ ]]]]      // 4 Stufen
+
+
+	var b1 = [0]			//  b1[0] = 0
+    var b2 = [[0]]
+    var b3 = [[[0]]]
+    var b4 = [[[[0]]]]
+	
+
+	var c1 = [1]			//  c1[0] = 1
+    var c2 = [[1]]
+    var c3 = [[[1]]]
+    var c4 = [[[[1]]]]   
+	
+	// c4[0][0][0]
+	
+	
+	
+	
+	
+	
+	// number relations...				x / 10 ....  x / 100 .... 
+	
+	var dez_10 = 7
+	var dez_100 = 75
+	
+	var x = make_number(10, 7)
+	
+	var xx = new number(10, 7)
+	
+	var xxx = new number(10, 7)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
     
     this.arraylist = [a1,a2,a3,a4]
     
